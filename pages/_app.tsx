396ca4307eda4f4f11eb0317ee00fa8891fcbe15
Shrysxs <shreyashborade2@gmail.com>
2025-09-08 23:55:49 +0530

    imported global styles in pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import type { AppProps } from "next/app";
 import { supabase } from '@/utils/supabase-client'
+import '@/styles/globals.css'
 
 function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
